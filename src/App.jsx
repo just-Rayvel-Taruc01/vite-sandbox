3231971eebd36c1c63823d0d4706b163c3cc50dd
Rayvel Taruc <rayvelm.taruc02@gmail.com>
2025-07-18 21:15:42 +0800

    Fix Refresh Error on wordle subfolder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import Wordle from "./wordle/Wordle";
 import Home from "./Home";
 import NotFound from "./pages/NotFound";
